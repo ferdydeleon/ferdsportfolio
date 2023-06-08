56893a55407ab4dbd40d6fedbc62920d300d63d7
Ferdinand De Leon <deleonferdinand30@gmail.com>
2023-06-08 23:02:09 +0800

    second commit

--- src/components/nav.jsx
+++ src/components/nav.jsx
@@ -1,12 +1,31 @@
-import React from 'react';
+import React, { useState } from "react";
+import { AiOutlineMenu,AiOutlineHome } from "react-icons/ai";
 
 const Nav = () => {
-    return (
-        <div>Side nave</div>
-   
-    )
-}
+  const [menu, setMenu] = useState(false);
 
+  const handleNav = () => {
+    setMenu(!menu);
+  };
 
-export default Nav
+  return (
+    <div>
+      <AiOutlineMenu
+        onClick={handleNav}
+        className="absolute top-4 right-4 z-[99] md:hidden"
+      />
+      {menu ? (
+        <div className="fixed w-full h-sceen bg-white/90 flex flex-col justify-center items-center z-20">
+          <a href="#main" className="w-[75%] flex justify-center items-center rounded-full shadow-lg bg-gray-100 shadow-gray-400 m-2 p-4 cursor-pointer hover:scale-110 ease-in duration-200">
+            <AiOutlineHome size={20} />
+            <span className="pl-4">Home</span>
+          </a>
+        </div>
+      ) : (
+        <div></div>
+      )}
+    </div>
+  );
+};
 
+export default Nav;
